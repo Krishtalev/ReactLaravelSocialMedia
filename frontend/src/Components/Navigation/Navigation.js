c8753100c25eae1d208827d364b49d59e4b5e0d0
NikitaKrishtalev <navyflash@yandex.ru>
2022-03-05 15:24:08 +0700

    force added frontend

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -2,9 +2,13 @@ import NavOption from "./NavOption/NavOption";
 import style from "./Navigation.module.css"
 
 const Navigation = () => {
+    let route = 'http://localhost:8000/api'
+    console.log("changed")
+    fetch(route).then(response => response.json())
+        .then(data => console.log(data));
     return (
         <footer className={style.homepageNav}>
-            <NavOption link="s" text="Friends"/>
+            <NavOption link="s" text="Friendss"/>
             <NavOption link="s" text="Messages"/>
             <NavOption link="s" text="News"/>
             <NavOption link="s" text="Settings"/>
